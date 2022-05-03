a2b4f77d92ee3bb9403c2789030c7eac39756d3c
VenturaDorantes <ventusDS@hotmail.com>
2022-05-03 15:27:35 -0500

    Agregando *Ordenando una lista de objetos* Ejemplo 2 - Semana 2

--- weekly_mission_2/1_operadores.js
+++ weekly_mission_2/1_operadores.js
@@ -115,3 +115,19 @@ console.log("Ejemplo 14: Alguno de los elementos en el array es false: " + areSo
 const products = ['Mil', 'Coffee', 'Sugar', 'Honey', 'Apple', 'Carrot']
 console.log("Ejemplo 15: Elementos ordenados usando SORT")
 console.log(products.sort())
+
+//Ejemplo 16: Ordenando una lista de objetos
+const users = [
+    { name: 'A', age: 150 },
+    { name: 'B', age: 50 },
+    { name: 'C', age: 100 },
+    { name: 'D', age: 22 },
+]
+
+users.sort((a, b) => { // podemos invocar una función
+    if (a.age < b.age) return -1
+    if (a.age > b.age) return 1
+})
+
+console.log("Ejemplo 16: Ordenando una lista de objetos por la edad")
+console.log(users) // sorted ascending
