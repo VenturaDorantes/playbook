17bd47b8b9221ffd73fafa7aec16f06383afdf96
VenturaDorantes <ventusDS@hotmail.com>
2022-05-03 15:21:26 -0500

    Agregando *Uso de sort para ordenar elementos* Ejemplo 2 - Semana 2

--- weekly_mission_2/1_operadores.js
+++ weekly_mission_2/1_operadores.js
@@ -110,3 +110,8 @@ const bools = [true, true, false, true]
 //Uso de Some para ver si al menos uno de los elementos es false
 const areSomeTrue = bools.some((b) => b === false)
 console.log("Ejemplo 14: Alguno de los elementos en el array es false: " + areSomeTrue) //True
+
+// Ejemplo 15: Uso de sort para ordenar elementos
+const products = ['Mil', 'Coffee', 'Sugar', 'Honey', 'Apple', 'Carrot']
+console.log("Ejemplo 15: Elementos ordenados usando SORT")
+console.log(products.sort())
