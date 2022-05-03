c9b269ca9c2907feacb7071fa84b80b777e642fd
VenturaDorantes <ventusDS@hotmail.com>
2022-05-03 12:32:50 -0500

    Agregando *Crear un objeto con propiedades* Ejemplo 1 - Semana 2

--- weekly_mission_2/0_objetos.js
+++ weekly_mission_2/0_objetos.js
@@ -4,3 +4,11 @@ console.log("Objetos")
 const myObject = {} //Esto es un objeto vacío
 console.log("Ejemplo 1: Crear un objeto vacío")
 console.log(myObject)
+
+//Ejemplo 2:Crear un objeto con propiedades
+const myObject2 = {
+    name: "Ventura",
+    age: 23
+}
+console.log("Ejemplo 2: Crear un objeto con propiedades")
+console.log(myObject2)
