6ced06b845899e759ae3e27a07d7ee7ae9a97719
VenturaDorantes <ventusDS@hotmail.com>
2022-05-03 14:29:56 -0500

    Agregando *Uso de filter para filtrar una lista* Ejemplo 2 - Semana 2

--- weekly_mission_2/1_operadores.js
+++ weekly_mission_2/1_operadores.js
@@ -41,3 +41,16 @@ const countriesFirstThreeLetters = countries6.map((country) =>
 )
 console.log("Ejemplo 6: Uso de map para convertir todos los nombres de una lista a mayúsculas")
 console.log(countriesFirstThreeLetters)
+
+//Ejemplo 7: Uso de filter para filtrar una lista de elementos
+const countries7 = ['Finlandia', 'Denmark', 'Sweden', 'Norway', 'Iceland']
+const countriesContainingLand = countries7.filter((country) => //esta es la función
+    country.includes('land') // indicación para solo filtrar elementos que incluyan "land"
+)
+console.log("Ejemplo 7: Uso de filter para filtrar una lista de elementos")
+console.log(countriesContainingLand)
+const countriesEndsByia = countries7.filter((country) => 
+    country.endsWith('ia')
+)
+console.log("Ejemplo 7: Países que terminan en ia")
+console.log(countriesEndsByia)
