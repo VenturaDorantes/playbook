70e3dfc5c7c96742519cd2cda39d121f550312b1
VenturaDorantes <ventusDS@hotmail.com>
2022-05-04 00:00:54 -0500

    Agregando *Método static* Ejemplo 3 - Semana 2

--- weekly_mission_2/2_clases_y_objetos.js
+++ weekly_mission_2/2_clases_y_objetos.js
@@ -125,3 +125,19 @@ missionCommanderNode.setLives = 3
 
 console.log(missionCommanderNode.getStudents) 
 console.log(missionCommanderNode.getLives) 
+
+// Ejemplo 8: Métodos static nos ayudan a escribir métodos en una clase que podemos usar sin necesidad de instanciar algún objeto
+class Toolbox {
+    static getMostUsefulTools(){
+        return["Command line", "git", "Text Editor"]
+    }
+}
+
+console.log("Ejemplo 8: Método static")
+// Puede llamar el método static directamente con el nombre de la clase
+console.log(Toolbox.getMostUsefulTools())
+// si intentamos instanciar un objeto, no podremos llamar este método static
+// const toolbox = new Toolbox()
+// console.log(toolbox.getMostUsefulTools()) // no es una función
+
+// HERENACIA: Nos permite relacionar clases entre sí y rehusar sus componentes
