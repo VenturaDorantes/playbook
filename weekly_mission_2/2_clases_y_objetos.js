4abd0d649f49edf9abf592ad3819ead8bdf28d8c
VenturaDorantes <ventusDS@hotmail.com>
2022-05-03 23:17:05 -0500

    Agregando *Métodos en los objetos* Ejemplo 3 - Semana 2

--- weekly_mission_2/2_clases_y_objetos.js
+++ weekly_mission_2/2_clases_y_objetos.js
@@ -27,3 +27,20 @@ class Student {
 const joseStudent = new Student ("Jose", 12, ["NodeJS","Python"])
 console.log("Ejemplo 3: Instanciar un objeto con atributos")
 console.log(joseStudent)
+
+// Ejemplo 4: Métodos en los objetos
+class Repository {
+    constructor(name, author, language, stars){
+        this.name = name
+        this.author = author
+        this.language = language
+        this.stars = stars
+    }
+
+    getInfo(){ // es una función que ejecutará cualquier objeto instanciado de esta clase
+        return `Repository ${this.name} has ${this.stars} stars`
+    }
+}
+console.log("Ejemplo 4: Métodos en los objetos")
+const myRepo = new Repository("LaunchX","VenturaDorantes","js",100)
+console.log(myRepo.getInfo())
