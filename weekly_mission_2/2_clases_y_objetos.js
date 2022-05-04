c9d5ea98438f8ac4f76c6c910b27ac9331fadb9f
VenturaDorantes <ventusDS@hotmail.com>
2022-05-03 23:07:40 -0500

    Agregando *Instancias a un objeto con atributos* Ejemplo 3 - Semana 2

--- weekly_mission_2/2_clases_y_objetos.js
+++ weekly_mission_2/2_clases_y_objetos.js
@@ -12,3 +12,18 @@ class Pet {
 const myPet1 = new Pet() // Puedo crear muchos objetos de la clase Pet
 console.log("Ejemplo 2: Crear un objeto a partir de una clase")
 console.log(myPet1) // Un objeto de la clase Pet
+
+// Ejemplo 3: Instancias un objeto con atributos
+class Student {
+    // El constructor nos permite instanciar un objeto y asignarle atributos, this nos ayuda a realizar esto.
+    constructor(name, age, subjects){
+        this.name = name
+        this.age= age
+        this.subjects = subjects
+    }
+}
+
+// Crear un objeto de la clase Student (esto se le llama instanciación)
+const joseStudent = new Student ("Jose", 12, ["NodeJS","Python"])
+console.log("Ejemplo 3: Instanciar un objeto con atributos")
+console.log(joseStudent)
