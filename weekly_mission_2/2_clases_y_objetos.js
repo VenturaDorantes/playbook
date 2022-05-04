57aaf960d9d038d31e5ba814a0da2d700dd6e2bc
VenturaDorantes <ventusDS@hotmail.com>
2022-05-03 21:47:30 -0500

    Agregando *Crear un objeto a partir de una clase* Ejemplo 3 - Semana 2

--- weekly_mission_2/2_clases_y_objetos.js
+++ weekly_mission_2/2_clases_y_objetos.js
@@ -1,6 +1,14 @@
-//Ejemplo 1: Crear un clase vacía
+// Ejemplo 1: Crear un clase vacía
 class Person {
 
 }
 console.log("Ejemplo 1: Crear una clase vacía")
 console.log(Person) // [class Person]
+
+// Ejemplo 2: Crear un objeto a partir de una clase
+class Pet {
+
+}
+const myPet1 = new Pet() // Puedo crear muchos objetos de la clase Pet
+console.log("Ejemplo 2: Crear un objeto a partir de una clase")
+console.log(myPet1) // Un objeto de la clase Pet
