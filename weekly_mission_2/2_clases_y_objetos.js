1b83af4c6f9f46203e26d564fe48f4246282c805
VenturaDorantes <ventusDS@hotmail.com>
2022-05-03 23:23:22 -0500

    Agregando *Atributos con valores por default* Ejemplo 3 - Semana 2

--- weekly_mission_2/2_clases_y_objetos.js
+++ weekly_mission_2/2_clases_y_objetos.js
@@ -44,3 +44,22 @@ class Repository {
 console.log("Ejemplo 4: Métodos en los objetos")
 const myRepo = new Repository("LaunchX","VenturaDorantes","js",100)
 console.log(myRepo.getInfo())
+
+// Ejemplo 5: Atributos con valores por default
+class PullRequest {
+    constructor(repo, title, lines_changed){
+        this.repo = repo
+        this.title = title
+        this.lines_changed = lines_changed
+        this.status = "OPEN"
+        this.dateCreated = new Date() // esto guardará la fecha actual en que se instancia el objeto
+    }
+
+    getInfo(){
+        return `This PR is in the repo: ${this.repo} (status: ${this.status}) and was created on ${this.dateCreated}`
+    }
+}
+
+console.log("Ejemplo 5: Atributos con valores por default")
+const myPR1 = new PullRequest("LaunchX","Mi Primer PR", 100)
+console.log(myPR1.getInfo())
