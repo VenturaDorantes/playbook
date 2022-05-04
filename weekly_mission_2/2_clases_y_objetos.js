8a9d9e0ca8376e714b5241d59f7f9a07caa19543
VenturaDorantes <ventusDS@hotmail.com>
2022-05-03 23:33:51 -0500

    Agregando *Getter para acceder a los atributos del objeto* Ejemplo 3 - Semana 2

--- weekly_mission_2/2_clases_y_objetos.js
+++ weekly_mission_2/2_clases_y_objetos.js
@@ -67,3 +67,22 @@ console.log(myPR1.getInfo())
 //Puedes instancias n cantidad de objetos de la misma clase
 const myPR2 = new PullRequest("LaunchX", "Mi Segundo PR", 99)
 console.log(myPR2.getInfo())
+
+// Ejemplo 6: Getters para acceder a los atributos del objeto
+class Ajolonauta {
+    constructor(name, age, stack){
+        this.name = name
+        this.age = age
+        this.stack = stack
+        this.exercises_completed = 0
+        this.exercises_todo = 99
+    }
+
+    get getExercisesCompleted(){
+        return this.exercises_completed
+    }
+}
+
+console.log("Ejemplo 6: Getters para acceder a los atributos del objeto")
+const woopa = new Ajolonauta("Woopa", 1, [])
+console.log(woopa.getExercisesCompleted)
