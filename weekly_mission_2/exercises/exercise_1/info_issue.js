105ff49296d363b36ffd62427661b4edc0394e73
VenturaDorantes <ventusDS@hotmail.com>
2022-05-11 05:31:10 -0500

    Agregando Ejercicio 1 - Semana 2 Informacion de Issue

---
+++ weekly_mission_2/exercises/exercise_1/info_issue.js
@@ -0,0 +1,27 @@
+const issue = {
+    title: "06 Live 3 Semana 2 [Jueves 14 de Abril]",
+    repositoryNameAssociated: "MissionNodeJS",
+    status: "Open",
+    numberOfComments: 64, 
+    labels: [
+        "FINALIZADO",
+        "LIVE 3",
+        "Semana-2"
+    ],
+    author: "carlogilmar",
+    dateCreated: "14-Abril-2022",
+    lastUpdated: "14-Abril-2022",
+
+    getTitleAndAuthor: function(){
+        return `El titulo del Issue es ${this.title} de ${this.author}`
+    },
+
+    getGeneralInfo: function(){
+        return `El Issue de ${this.title} fue creado el dia ${this.dateCreated} con los labels de ${this.labels}`
+    }
+}
+
+console.log("Repositorio asociado: " + issue.repositoryNameAssociated)
+console.log("Issues status: " + issue.status)
+console.log(issue.getTitleAndAuthor())
+console.log(issue.getGeneralInfo())
