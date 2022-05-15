1d0c033992d55e33003d9ef75e993d5cdfd9bd3d
VenturaDorantes <ventusDS@hotmail.com>
2022-05-15 15:16:49 -0500

    Editando Info de biografia de Facebook

--- weekly_mission_2/exercises/exercise_1/Facebook/info_biography.js
+++ weekly_mission_2/exercises/exercise_1/Facebook/info_biography.js
@@ -1,10 +1,17 @@
 const biography = {
     user: "Ventura DS",
+    gender: "Hombre",
     biography: "Hola estoy aprendiendo sobre NodeJS",
+    live: "Estado de México",
 
     getUserAndBiography: function(){
         return `El usuario ${this.user} tiene como biografia ${this.biography}`
+    },
+
+    getUserAndLive: function(){
+        return `El usuario ${this.user} vive en ${this.live}`
     }
 }
 
 console.log(biography.getUserAndBiography())
+console.log(biography.getUserAndLive())
