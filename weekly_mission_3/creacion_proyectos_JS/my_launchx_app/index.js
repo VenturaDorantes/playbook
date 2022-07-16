c0212c7e36fde38f006c051730269104405f4a91
VenturaDorantes <ventusDS@hotmail.com>
2022-07-16 15:05:59 -0500

    Creacion archivo index para llamar la clase MissionCommander

---
+++ weekly_mission_3/creacion_proyectos_JS/my_launchx_app/index.js
@@ -0,0 +1,9 @@
+const MissionCommander = require('./app/missionCommander.js');
+
+const jose = new MissionCommander("Jose")
+// const ventura = new MissionCommander("Ventura")
+// const carlo = new MissionCommander("Carlo")
+
+console.log(jose.name)
+// console.log(ventura.name)
+// console.log(carlo.name)
