dae5e26eb28d0a442b6d8b25546761ac7bb4b26d
VenturaDorantes <ventusDS@hotmail.com>
2022-07-16 15:10:59 -0500

    Agregando 2 nuevos objetos

--- weekly_mission_3/creacion_proyectos_JS/my_launchx_app/index.js
+++ weekly_mission_3/creacion_proyectos_JS/my_launchx_app/index.js
@@ -1,9 +1,9 @@
 const MissionCommander = require('./app/missionCommander.js');
 
 const jose = new MissionCommander("Jose")
-// const ventura = new MissionCommander("Ventura")
-// const carlo = new MissionCommander("Carlo")
+const ventura = new MissionCommander("Ventura")
+const carlo = new MissionCommander("Carlo")
 
 console.log(jose.name)
-// console.log(ventura.name)
-// console.log(carlo.name)
+console.log(ventura.name)
+console.log(carlo.name)
