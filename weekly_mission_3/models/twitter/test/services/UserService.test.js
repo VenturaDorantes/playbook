c44f615e0fdb103a88864bc6ff369715cd877457
VenturaDorantes <ventusDS@hotmail.com>
2022-07-16 22:58:41 -0500

    Creando prueba de unidad para obtener datos de usuario en una lista

--- weekly_mission_3/models/twitter/test/services/UserService.test.js
+++ weekly_mission_3/models/twitter/test/services/UserService.test.js
@@ -9,4 +9,14 @@ describe("Tests para UserService", () => {
         expect(user.id).toBe(1)
         expect(user.bio).not.toBeUndefined()
     });
+
+    test("2) Obtener todos los datos de usuario en una lista", () => {
+        const user = UserService.create(1, "venturadosa", "ventura")
+        const userInfoInList = UserService.getInfo(user)
+
+        expect(userInfoInList[0]).toBe(1)
+        expect(userInfoInList[1]).toBe("venturadosa")
+        expect(userInfoInList[2]).toBe("ventura")
+        expect(userInfoInList[3]).toBe("Sin Bio")
+    })
 })
