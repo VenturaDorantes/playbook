1737f72cb1b9937ddcf153eaba48705498e4037d
VenturaDorantes <ventusDS@hotmail.com>
2022-07-16 20:02:23 -0500

    Modificando prueba de unidad para la clase de Spiderman

--- weekly_mission_3/test_driven_development/spiderverse/test/spiderman.test.js
+++ weekly_mission_3/test_driven_development/spiderverse/test/spiderman.test.js
@@ -1,6 +1,17 @@
-describe("Descripción ficticia de la suite de pruebas", () => {
-    test('Case 1 Dummy', () => {
-        const resultOfSomething = 1 + 2
-        expect(resultOfSomething).toBe(18);
+const Spiderman = requiere('./../app/Spiderman')
+
+describe("Prueba de unidad para la clase Spiderman", () => {
+    test('1) Creando objeto para spiderman', () => {
+        // Aquí escribimos el código que queremos usar tal cuál
+        // Quiero poder instanciar un objeto Spiderman con esta información
+        const andrewGarfield = new Spiderman("Spiderman Sony", 31, "Andrew Garfield", 2, "Sony")
+
+        // Validamos que este código funcione de la forma esperada
+
+        expect(andrewGarfield.name).toBe("Spiderman Sony")
+        expect(andrewGarfield.age).toBe(31)
+        expect(andrewGarfield.actor).toBe("Andrew Garfield")
+        expect(andrewGarfield.movies).toBe(2)
+        expect(andrewGarfield.studio).toBe("Sony")
     });
 })
