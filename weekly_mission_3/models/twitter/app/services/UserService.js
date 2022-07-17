040ecafb8639b00c20c8502cac75c8c98497346d
VenturaDorantes <ventusDS@hotmail.com>
2022-07-16 23:03:05 -0500

    Creando funcion getInfo para la clase UserService

--- weekly_mission_3/models/twitter/app/services/UserService.js
+++ weekly_mission_3/models/twitter/app/services/UserService.js
@@ -2,7 +2,18 @@ const User = require('./../models/User')
 
 class UserService {
     static create(id, username, name){
-        return new User(id, username, name, "Sin bio")
+        return new User(id, username, name, "Sin Bio")
+    }
+
+    static getInfo(user){
+        var info = []
+
+        info.push(user.id)
+        info.push(user.username)
+        info.push(user.name)
+        info.push(user.bio)
+
+        return info
     }
 }
 
