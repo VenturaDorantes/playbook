5c4acc319398b07845f5cd168163d3d3c1466940
VenturaDorantes <ventusDS@hotmail.com>
2022-07-19 20:56:22 -0500

    Agregando prueba unitaria3 para UserView de Twitter

--- weekly_mission_3/models/twitter/test/views/UserView.test.js
+++ weekly_mission_3/models/twitter/test/views/UserView.test.js
@@ -10,10 +10,17 @@ describe("Test for UserView", () => {
         expect(result.error).toMatch(/payload no existe/)
     });
 
-    test("Return an error object when try to create a new user with payload with invalid properties", () => {
+    test("Devuelve un objeto de error cuando intenta crear un nuevo usuario con carga útil con propiedades no válidas", () => {
         const payload = {username: null, name: 12, id: "id"}
         const result = UserView.createUser(payload)
 
         expect(result.error).toMatch(/necesitan tener un valor válido/)
-    })
+    });
+
+    test("Devuelve un objeto de error al intentar crear un nuevo usuario con una carga útil con propiedades faltantes", () => {
+        const payload = {username: "Username"}
+        const result = UserView.createUser(payload)
+
+        expect(result.error).toMatch(/necesitan tener un valor válido/)
+    });
 })
