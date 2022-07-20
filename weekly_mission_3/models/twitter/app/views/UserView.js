3d2135be81c628519ffd89d38a233a0ab296c599
VenturaDorantes <ventusDS@hotmail.com>
2022-07-19 20:50:11 -0500

    Agregando solucion prueba unitaria2 para twitter

--- weekly_mission_3/models/twitter/app/views/UserView.js
+++ weekly_mission_3/models/twitter/app/views/UserView.js
@@ -5,6 +5,17 @@ class UserView {
         if (payload == null){
             return { error: 'payload no existe'}
         }
+
+        var ret = ((payload.username == null) ||
+                    (typeof payload.username !== 'string') ||
+                    (payload.name == null) ||
+                    (typeof payload.name !== 'string') ||
+                    (payload.id == null) ||
+                    (Number.isInteger(payload.id) == false))
+
+        if (ret){
+            return { error: 'necesitan tener un valor válido' }
+        }
     }
 }
 
