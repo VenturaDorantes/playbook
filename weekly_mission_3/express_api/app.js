cfa2ff9e876c1d981112346820b49ff34e630fb6
VenturaDorantes <ventusDS@hotmail.com>
2022-07-22 13:58:17 -0500

    Corriguiendo error de DELETE

--- weekly_mission_3/express_api/app.js
+++ weekly_mission_3/express_api/app.js
@@ -44,7 +44,7 @@ app.put('/v1/explorers/:id', (req, res) => {
     res.status(200).json({message: "Updated!"})
 })
 
-app-delete('/v1/explorers/:id', (req, res) => {
+app.delete('/v1/explorers/:id', (req, res) => {
     console.log(`Api Explorers DELETE request ${new Date()}`)
     console.log(`Delete explroers with id ${req.params.id}`)
 
