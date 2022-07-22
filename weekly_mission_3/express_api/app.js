e46fae75a1057ebadd9fb01584aa9faf0ba3fa7c
VenturaDorantes <ventusDS@hotmail.com>
2022-07-22 13:55:35 -0500

    DELETE crea un endpoint pra eliminar un explorer

--- weekly_mission_3/express_api/app.js
+++ weekly_mission_3/express_api/app.js
@@ -44,6 +44,14 @@ app.put('/v1/explorers/:id', (req, res) => {
     res.status(200).json({message: "Updated!"})
 })
 
+app-delete('/v1/explorers/:id', (req, res) => {
+    console.log(`Api Explorers DELETE request ${new Date()}`)
+    console.log(`Delete explroers with id ${req.params.id}`)
+
+    const requestBody = req.body // Parámetros de un cliente
+    res.status(200).json({message: "Deleted"})
+})
+
 // Con esto iniciamos esta app
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
