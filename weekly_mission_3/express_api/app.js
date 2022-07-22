241a18ee4cd51bbb556818f8e93e47ab03bb1c38
VenturaDorantes <ventusDS@hotmail.com>
2022-07-22 13:39:58 -0500

    Agregando GET cre un endpoint que regrese un explorer mediando un ID

--- weekly_mission_3/express_api/app.js
+++ weekly_mission_3/express_api/app.js
@@ -12,13 +12,21 @@ const port = 3000
 app.get('/v1/explorers', (req, res) => {
     console.log(`Api Explorers GET ALL requests ${new Date()}`)
     
-    const explorer1 = {id: 1, name: "Carlo1"}
-    const explorer2 = {id: 2, name: "Carlo2"}
-    const explorer3 = {id: 3, name: "Carlo3"}
-    const explorer4 = {id: 4, name: "Carlo4"}
+    const explorer1 = {id: 1, name: "Ventura1"}
+    const explorer2 = {id: 2, name: "Ventura2"}
+    const explorer3 = {id: 3, name: "Ventura3"}
+    const explorer4 = {id: 4, name: "Ventura4"}
 
     const explorers = [explorer1, explorer2, explorer3, explorer4]
     res.status(200).json(explorers)
+})
+
+app.get('/v1/explorers/:id', (req, res) => {
+    console.log(`Api Explorers GET request ${new Date()}`)
+    console.log(`Getting explorer with id ${req.params.id}`)
+
+    const explorer = {id: 1, name: "Ventura"}
+    res.status(200).json(explorer)
 })
 
 // Con esto iniciamos esta app
