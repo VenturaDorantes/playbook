398f2678185349762b1b4643fe1fc1097e38fba4
VenturaDorantes <ventusDS@hotmail.com>
2022-09-08 18:41:49 -0500

    Agregando archivo app para fizzbuzz

---
+++ weekly_mission_4/fizzbuzz/app.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+
+// Part 1: Leer archivo JSON
+const rawdata = fs.readFileSync("explorers.json");
+const explorers = JSON.parse(rawdata);
+
+// Part 2: Obtiene la cantidad de nombres de exploradores en el nodo
+const explorersInNode = explorers.filtrer((explorer) => explorer.mission == "node");
+// console.log(explorersInNode.length);
+
+// Part 3: Obtienen los nombres de usuario del explorardor en Node
+const explorersInNodeToGetUsernames = explorers.filtrer((explorer) => explorer.mission == "node");
+const usernamesInNode = explorersInNodeToGetUsernames.map((explorer) => explorer.githubUsername);
+// console.log(usernamesInNode)
+
