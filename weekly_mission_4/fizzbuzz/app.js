9f8bb5c72be7940e30fdbf29672bfcbb6a954130
VenturaDorantes <ventusDS@hotmail.com>
2022-09-08 19:43:00 -0500

    Agregando clase de fizzbuzzService en app de fizzbuzz

--- weekly_mission_4/fizzbuzz/app.js
+++ weekly_mission_4/fizzbuzz/app.js
@@ -1,26 +1,28 @@
 // Clases que se utilizaran
 const Reader = require("./lib/utils/Reader");
 const ExplorerService = require("./lib/services/ExplorerService");
+const FizzbuzzService = require("./lib/services/FizzbuzzService");
 
-
-const fs = require("fs");
 
 // Part 1: Leer archivo JSON =================
 const explorers = Reader.readJsonFile("explorers.json");
-
-// const rawdata = fs.readFileSync("explorers.json");
-// const explorers = JSON.parse(rawdata);
 
 // Part 2: Obtiene la cantidad de nombres de exploradores en el nodo ===============
 const explorersInNode = ExplorerService.filterByMission(explorers, "node");
 const amountOfExplorers = ExplorerService.getAmountOfExplorersByMission(explorers, "node");
 const usernamesInNode = ExplorerService.getExplorersUsernamesByMission(explorers, "node");
 
-// const explorersInNode = explorers.filtrer((explorer) => explorer.mission == "node");
-// console.log(explorersInNode.length);
+console.log(usernamesInNode)
 
-// Part 3: Obtienen los nombres de usuario del explorardor en Node =================
-const explorersInNodeToGetUsernames = explorers.filtrer((explorer) => explorer.mission == "node");
-// const usernamesInNode = explorersInNodeToGetUsernames.map((explorer) => explorer.githubUsername);
-// console.log(usernamesInNode)
+const explorer1 = { name: "Explorer1", score: 1}
+console.log(FizzbuzzService.applyValidationInExplorer(explorer1))  // {name: "Explorer1", score: 1, trick: 1} 
+
+const explorer3 = {name: "Explorer3", score: 3}
+console.log(FizzbuzzService.applyValidationInExplorer(explorer3)) // {name: "Explorer3", score: 3, trick: "FIZZ"}
+
+const explorer5 = {name: "Explorer5", score: 5}
+console.log(FizzbuzzService.applyValidationInExplorer(explorer5)) // {name: "Explorer5", score: 5, trick: "BUZZ"}
+
+const explorer15 = {name: "Explorer15", score: 15}
+console.log(FizzbuzzService.applyValidationInExplorer(explorer15)) // {name: "Explorer15", score: 15, trick: "FIZZBUZZ"}
 
