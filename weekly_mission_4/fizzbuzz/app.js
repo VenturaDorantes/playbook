fe20142a03f05819e46815862d4e0f974247199b
VenturaDorantes <ventusDS@hotmail.com>
2022-09-08 19:05:20 -0500

    Agregando el llamado de la clase Reader en app.js de fizzbuzz

--- weekly_mission_4/fizzbuzz/app.js
+++ weekly_mission_4/fizzbuzz/app.js
@@ -1,14 +1,20 @@
+
+// Clases que se utilizaran
+const Reader = require("./lib/utils/Reader");
+
 const fs = require("fs");
 
-// Part 1: Leer archivo JSON
-const rawdata = fs.readFileSync("explorers.json");
-const explorers = JSON.parse(rawdata);
+// Part 1: Leer archivo JSON =================
+const explorers = Reader.readJsonFile("explorers.json");
 
-// Part 2: Obtiene la cantidad de nombres de exploradores en el nodo
+// const rawdata = fs.readFileSync("explorers.json");
+// const explorers = JSON.parse(rawdata);
+
+// Part 2: Obtiene la cantidad de nombres de exploradores en el nodo ===============
 const explorersInNode = explorers.filtrer((explorer) => explorer.mission == "node");
 // console.log(explorersInNode.length);
 
-// Part 3: Obtienen los nombres de usuario del explorardor en Node
+// Part 3: Obtienen los nombres de usuario del explorardor en Node =================
 const explorersInNodeToGetUsernames = explorers.filtrer((explorer) => explorer.mission == "node");
 const usernamesInNode = explorersInNodeToGetUsernames.map((explorer) => explorer.githubUsername);
 // console.log(usernamesInNode)
