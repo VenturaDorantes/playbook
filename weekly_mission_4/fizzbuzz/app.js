9fcad7fd649223486f07a646682f45362cca5465
VenturaDorantes <ventusDS@hotmail.com>
2022-09-08 19:31:44 -0500

    Agregando la nueva clase de ExplorerService en app.js de fizzbuzz

--- weekly_mission_4/fizzbuzz/app.js
+++ weekly_mission_4/fizzbuzz/app.js
@@ -1,6 +1,7 @@
-
 // Clases que se utilizaran
 const Reader = require("./lib/utils/Reader");
+const ExplorerService = require("./lib/services/ExplorerService");
+
 
 const fs = require("fs");
 
@@ -11,11 +12,15 @@ const explorers = Reader.readJsonFile("explorers.json");
 // const explorers = JSON.parse(rawdata);
 
 // Part 2: Obtiene la cantidad de nombres de exploradores en el nodo ===============
-const explorersInNode = explorers.filtrer((explorer) => explorer.mission == "node");
+const explorersInNode = ExplorerService.filterByMission(explorers, "node");
+const amountOfExplorers = ExplorerService.getAmountOfExplorersByMission(explorers, "node");
+const usernamesInNode = ExplorerService.getExplorersUsernamesByMission(explorers, "node");
+
+// const explorersInNode = explorers.filtrer((explorer) => explorer.mission == "node");
 // console.log(explorersInNode.length);
 
 // Part 3: Obtienen los nombres de usuario del explorardor en Node =================
 const explorersInNodeToGetUsernames = explorers.filtrer((explorer) => explorer.mission == "node");
-const usernamesInNode = explorersInNodeToGetUsernames.map((explorer) => explorer.githubUsername);
+// const usernamesInNode = explorersInNodeToGetUsernames.map((explorer) => explorer.githubUsername);
 // console.log(usernamesInNode)
 
